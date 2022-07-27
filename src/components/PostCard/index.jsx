f51ff4f0763206a5d28152fde83ce0c62682e0b7
brenxribas <breno_ribas@outlook.com>
2022-07-27 12:33:18 -0300

    funcionamento searchbar

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,8 +1,10 @@
-export const PostCard = ({ title, cover, body}) => (
+import './styles.css';
+
+export const PostCard = ({id, title, cover, body}) => (
     <div className="post">
         <img src={cover} alt={title} />
         <div className='post-content'>
-            <h1>{title}</h1>
+            <h1>{title} {id} </h1>
             <p>{body}</p>
         </div>
     </div>
